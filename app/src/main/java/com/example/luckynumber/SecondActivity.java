bb853b5659c76864f94c96b8840b412f82ef7a8b
nguetcheu <dnguetcheu@gmail.com>
2024-11-23 22:12:09 +0100

    Envoi du message depuis la second activité et création du nombre aléatoire

--- app/src/main/java/com/example/luckynumber/SecondActivity.java
+++ app/src/main/java/com/example/luckynumber/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.example.luckynumber;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class SecondActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView luckyNumberTxt;
+    TextView welcomeTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,44 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        welcomeTxt = findViewById(R.id.textView2);
+        btn = findViewById(R.id.share_btn);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+
+        // Receiving data from the Main activity
+        Intent i = getIntent();
+        String userName = i.getStringExtra("userName");
+
+        // Generating Random numbers;
+        int random_num = generateRandomNumber();
+        luckyNumberTxt.setText(""+random_num);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareData(userName, random_num);
+            }
+        });
+    }
+
+    public int generateRandomNumber() {
+        Random random = new Random();
+        int upper_limit = 1000;
+
+        int randomNumberGenerated = random.nextInt(upper_limit);
+        return randomNumberGenerated;
+    }
+
+    public void shareData(String userName, int random_num){
+        // Implicit intent
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/Plain");
+
+        // Additional info
+        i.putExtra(Intent.EXTRA_SUBJECT, userName + " got lucky today");
+        i.putExtra(Intent.EXTRA_TEXT, " His lucky number is "+random_num);
+
+        startActivity(Intent.createChooser(i, "Choose a Platform"));
     }
 }
